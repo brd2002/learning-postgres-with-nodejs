6f3c2cb455ec2b2de9c9cdfea4ebaf9f3c5738a9
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-03-01 19:12:45 +0530

    find user using id is done

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const client =  new Client({
     connectionString : important.databaseUrl // database link from important.ts file neondb
 })
 
+// this function will create a table in the database
 async function createUsersTable(){
     await client.connect();
     const result = await client.query(`
@@ -17,6 +18,7 @@ async function createUsersTable(){
         `);
         console.log(result);
 }
+// this function will insert a user in the database
 async function insertUser(username: string, email: string, password: string){
     await client.connect();
     const result = await client.query(`
@@ -24,6 +26,7 @@ async function insertUser(username: string, email: string, password: string){
     `);    
     console.log(result);
 }
+// this function will get all the users from the database
 async function getUsers(){
     await client.connect();
     const result = await client.query(`
@@ -31,6 +34,15 @@ async function getUsers(){
     `);
     console.log(result.rows);
 }
+// this function will get a user by id from the database
+async function getUserById(id: number){
+    await client.connect();
+    const result = await client.query(`
+        select * from all_users where id = ${id};
+    `);
+    console.log(result.rows);
+}
 // createUsersTable();
 // insertUser('anusri ruidas', 'anu@gmail.com', '123456');
-getUsers();
+// getUsers();
+// getUserById(1);
